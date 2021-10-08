a8843b0b4d6c832ec520f931018f0369dc34bbd5
matcasti <matcasti@umag.cl>
2021-10-08 08:55:28 -0300

    Pronto a ser descontinuado

--- script/proyectos_anid/4-comparacion_dbs.R
+++ script/proyectos_anid/4-comparacion_dbs.R
@@ -1,17 +1,17 @@
-
-# ANID historicos proyectos adjudicados (modificado) ----
-
-library(data.table)
-
-source("script/proyectos_anid/1-import.R")
-source("script/proyectos_anid/2-normalize.R")
-source("script/proyectos_anid/3-db_modificada.R")
-
-# Todos los códigos en mz_austral que no estén en dbp_modificado
-mz_austral[!mz_austral$codigo_proyecto %in% dbp_modificado$codigo_proyecto]
-
-# Todos los códigos en dbp_modificado que no estén en mz_austral
-dbp_modificado[!dbp_modificado$codigo_proyecto %in% mz_austral$codigo_proyecto]
-
-  ## Debiesen aparecer 4 proyectos 2021, ya que estos al estar agregados manualmente
-  ## es esperable que no esten en la base de datos original
+# 
+# # ANID historicos proyectos adjudicados (modificado) ----
+# 
+# library(data.table)
+# 
+# source("script/proyectos_anid/1-import.R")
+# source("script/proyectos_anid/2-normalize.R")
+# source("script/proyectos_anid/3-db_modificada.R")
+# 
+# # Todos los códigos en mz_austral que no estén en dbp_modificado
+# mz_austral[!mz_austral$codigo_proyecto %in% dbp_modificado$codigo_proyecto]
+# 
+# # Todos los códigos en dbp_modificado que no estén en mz_austral
+# dbp_modificado[!dbp_modificado$codigo_proyecto %in% mz_austral$codigo_proyecto]
+# 
+#   ## Debiesen aparecer 4 proyectos 2021, ya que estos al estar agregados manualmente
+#   ## es esperable que no esten en la base de datos original
