5520de36d1c375d97e2131c3f59e489f14c8105b
matcasti <matcasti@umag.cl>
2021-10-22 10:15:49 -0300

    graficos de los productos a revisar

--- script/lab_instrumentos/2-clustering.R
+++ script/lab_instrumentos/2-clustering.R
@@ -24,9 +24,8 @@
 
 # Importar datos ------------------------------------------------------------------------------
 
-  lab_instrumentosANID <- lab_instrumentosANID[j = norm_problema := gsub(pattern = "macrozona-austral", 
-                                                 replacement = "", 
-                                                 x = norm_problema)]
+  lab_instrumentosANID <- readRDS(file = "data/lab_instrumentos/clean/data.RDS") |> 
+    .s(j = norm_problema := gsub(pattern = "macrozona-austral", replacement = "", x = norm_problema))
   
 # Preparación del corpus ----------------------------------------------------------------------
 
@@ -34,130 +33,13 @@
   m <- tm::Corpus(x = tm::VectorSource(x = unique(lab_instrumentosANID$norm_problema) ) ) |> 
     tm::TermDocumentMatrix(control = list(minWordLength = c(1, Inf) ) ) |> 
     as.matrix()
-  
-  
-  
 
 # EDA -----------------------------------------------------------------------------------------
 
   if (FALSE) {
     local({
-      ## Network Análisis
-      m2 <- m[rowSums(m) > 1, ]
-      m2[m2 > 1] <- 1
-      termM <- m2 %*% t(m2)
-      g <- igraph::graph.adjacency(termM, weighted = T, mode = 'undirected')
-      g <- igraph::simplify(g)
-      igraph::V(g)$label <- igraph::V(g)$name
-      igraph::V(g)$degree <- igraph::degree(g)
       
-      # Histogram of node degree
-      hist(igraph::V(g)$degree,
-           breaks = 20,
-           col = 'green',
-           main = 'Histogram of Node Degree',
-           ylab = 'Frequency',
-           xlab = 'Degree of Vertices')
-      
-      # Network diagram
-      set.seed(222)
-      plot(g, vertex.size = 4)
-      
-      # Detección de comunidades ----
-      comm <- igraph::cluster_edge_betweenness(g)
-      plot(comm, g, vertex.size = 4)
-      net_k1 <- igraph::membership(comm)
-      net_k1_n <- names(net_k1)
-      
-      # ## Asignamos grupos del clustering ----
-      # for (i in net_k1_n) {
-      #   # Identificando el grupo dentro del dendrograma
-      #   k <- net_k1[net_k1_n == i][[1]]
-      #   # Asignar el grupo a una nueva columna
-      #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k1 := as.numeric(k)]
-      # }
-      
-      prop <- igraph::cluster_label_prop(g)
-      plot(prop, g, vertex.size = 4)
-      net_k2 <- igraph::membership(prop)
-      net_k2_n <- names(net_k2)
-      
-      # ## Asignamos grupos del clustering ----
-      # for (i in net_k2_n) {
-      #   # Identificando el grupo dentro del dendrograma
-      #   k <- net_k2[net_k2_n == i][[1]]
-      #   # Asignar el grupo a una nueva columna
-      #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k2 := as.numeric(k)]
-      # }
-      
-      set.seed(222)
-      greed <- igraph::cluster_fast_greedy(igraph::as.undirected(g))
-      plot(greed, igraph::as.undirected(g), vertex.size = 4)
-      net_k3 <- igraph::membership(greed)
-      net_k3_n <- names(net_k3)
-      
-      # ## Asignamos grupos del clustering ----
-      # for (i in net_k3_n) {
-      #   # Identificando el grupo dentro del dendrograma
-      #   k <- net_k3[net_k3_n == i][[1]]
-      #   # Asignar el grupo a una nueva columna
-      #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k3 := as.numeric(k)]
-      # }
-      
-      # Hub and authorities
-      hs <- igraph::hub_score(g, weights = NA)$vector
-      as <- igraph::authority_score(g, weights = NA)$vector
-      par(mfrow = c(1,2))
-      plot(g, vertex.size = hs * 10, main = 'Hubs',
-           vertex.color = rainbow(50))
-      plot(g, vertex.size = as * 9, main = 'Authorities',
-           vertex.color = rainbow(50))
-      par(mfrow = c(1,1))
-          # Highlighting degrees
-      igraph::V(g)$label.cex <- 2.2 * igraph::V(g)$degree / max(igraph::V(g)$degree) + 0.3
-          igraph::V(g)$label.color <- rgb(0, 0, .2, .8)
-          igraph::V(g)$frame.color <- NA
-          egam <- (log(igraph::E(g)$weight) + .4) / max(log(igraph::E(g)$weight) + .4)
-          igraph::E(g)$color <- rgb(.5, .5, 0, egam)
-          igraph::E(g)$width <- egam
-          plot(g, vertex.size = igraph::V(g)$degree * .5)
-          
-          # Network of tweets
-          m3 <- t(m2) %*% m2
-          g <- igraph::graph.adjacency(m3, weighted = T, mode = 'undirected')
-          igraph::V(g)$degree <- igraph::degree(g)
-          g <- igraph::simplify(g)
-          hist(igraph::V(g)$degree,
-               breaks = 100,
-               col = 'green',
-               main = 'Histogram of Degree',
-               ylab = 'Freuqency',
-               xlab = 'Degree')
-          
-          # Set labels of vertices to tweet IDs
-          igraph::V(g)$label <- igraph::V(g)$name
-          igraph::V(g)$label.cex <- 1
-          igraph::V(g)$label.color <- rgb(.4, 0, 0, .7)
-          igraph::V(g)$size <- 2
-          igraph::V(g)$frame.color <- NA
-          plot(g, vertex.size = 3)
-          
-          # Delete vertices
-          egam <- (log(igraph::E(g)$weight) + .2) / max(log(igraph::E(g)$weight) + .2)
-          igraph::E(g)$color <- rgb(.5, .5, 0, egam)
-          igraph::E(g)$width <- egam
-          g2 <- igraph::delete.vertices(g, igraph::V(g)[igraph::degree(g) < 2])
-          plot(g2,
-               vertex.label.cex = .9,
-               vertex.label.color = 'black')
-          
-          # Delete edges
-          igraph::E(g)$color <- rgb(.5, .5, 0, egam)
-          igraph::E(g)$width <- egam
-          g3 <- igraph::delete.edges(g, igraph::E(g)$weight <- 1)
-          g3 <- igraph::delete.vertices(g3, igraph::V(g3)[igraph::degree(g3) < 20])
-          plot(g3)
-          })
+      })
   }
   
 # Técnicas de clustering ----------------------------------------------------------------------
---
+++ script/lab_instrumentos/productos/grafico_network.R
@@ -0,0 +1,148 @@
+## función: Generar graficos de redes de términos
+## fecha: 22-oct
+
+# Preparar el espacio de trabajo --------------------------------------------------------------
+
+library(tm);
+library(igraph);
+library(data.table);
+.s <- function(x, ...) {
+  stopifnot(inherits(x, "data.table"))
+  temp <- substitute(x[...])
+  eval(temp)
+}
+
+# Importar los datos --------------------------------------------------------------------------
+
+data <- readRDS(file = "data/lab_instrumentos/clean/data.RDS") |> 
+  # Eliminamos el término macrozona-austral por ser muy influyente en el modelado
+  .s(j = norm_problema := gsub(pattern = "macrozona-austral", replacement = "", x = norm_problema));
+
+
+# Pre-procesamiento ---------------------------------------------------------------------------
+
+## Creación de un corpus para posterior análisis
+m <- tm::Corpus(x = tm::VectorSource(x = unique(data$norm_problema) ) ) |> 
+  tm::TermDocumentMatrix(control = list(minWordLength = c(1, Inf) ) ) |> 
+  as.matrix();
+
+## Obtenemos aquellos términos que aparezcan almenos dos veces entre los problemas únicos
+m2 <- m[rowSums(m) > 1, ];
+
+## Y a aquellos problemas les asignamos el valor de 1 (es decir, se encuentra presente o no)
+m2[m2 > 1] <- 1;
+
+## Calculamos la co-ocurrencia
+termM <- m2 %*% t(m2);
+
+## Creamos los nodos con sus respectivos vertices
+g <- igraph::graph.adjacency(termM, weighted = T, mode = 'undirected') |> 
+  igraph::simplify();
+
+## Le asignamos etiquetas a los nodos para mejor visualización
+igraph::V(g)$label <- igraph::V(g)$name;
+
+## Le asignamos la propiedad degree (número de vertices adjacentes) al grafo
+igraph::V(g)$degree <- igraph::degree(g);
+
+# Producto ------------------------------------------------------------------------------------
+
+## Histogram of node degree -------------------------------------------------------------------
+local({
+  hist(igraph::V(g)$degree,
+       col = 'green4',
+       main = 'Histogram of Node Degree',
+       ylab = 'Frequency',
+       xlab = 'Degree of Vertices')
+});
+
+## Network diagram ----------------------------------------------------------------------------
+local({
+  set.seed(222)
+  plot(g, vertex.size = 4)
+});
+
+## Detacción de comunidades -------------------------------------------------------------------
+
+### Structure detection based on edge betweenness ---------------------------------------------
+pdf(file = "output/lab_instrumentos/productos/network_1.pdf", width = 8, height = 8);
+local({
+  comm <- igraph::cluster_edge_betweenness(g)
+  plot(comm, g, vertex.size = 4)
+  net_k1 <- igraph::membership(comm)
+  net_k1_n <- names(net_k1)
+  
+  # ## Asignamos grupos del clustering ----
+  # for (i in net_k1_n) {
+  #   # Identificando el grupo dentro del dendrograma
+  #   k <- net_k1[net_k1_n == i][[1]]
+  #   # Asignar el grupo a una nueva columna
+  #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k1 := as.numeric(k)]
+  # }
+});
+dev.off();
+
+### Structure detection based on propagating labels -------------------------------------------
+pdf(file = "output/lab_instrumentos/productos/network_2.pdf", width = 8, height = 8);
+local({
+  prop <- igraph::cluster_label_prop(g)
+  plot(prop, g, vertex.size = 4)
+  net_k2 <- igraph::membership(prop)
+  net_k2_n <- names(net_k2)
+  
+  # ## Asignamos grupos del clustering ----
+  # for (i in net_k2_n) {
+  #   # Identificando el grupo dentro del dendrograma
+  #   k <- net_k2[net_k2_n == i][[1]]
+  #   # Asignar el grupo a una nueva columna
+  #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k2 := as.numeric(k)]
+  # }
+});
+dev.off();
+
+### Structure detection via greedy optimization of modularity ---------------------------------
+pdf(file = "output/lab_instrumentos/productos/network_3.pdf", width = 8, height = 8);
+local({
+  set.seed(222)
+  greed <- igraph::cluster_fast_greedy(igraph::as.undirected(g))
+  plot(greed, igraph::as.undirected(g), vertex.size = 4)
+  net_k3 <- igraph::membership(greed)
+  net_k3_n <- names(net_k3)
+  
+  # ## Asignamos grupos del clustering ----
+  # for (i in net_k3_n) {
+  #   # Identificando el grupo dentro del dendrograma
+  #   k <- net_k3[net_k3_n == i][[1]]
+  #   # Asignar el grupo a una nueva columna
+  #   lab_instrumentosANID[norm_problema %like% i, norm_problema_net_k3 := as.numeric(k)]
+  # }
+});
+dev.off();
+
+
+
+
+
+
+
+
+
+# Revisar después
+
+# # Hub and authorities
+# hs <- igraph::hub_score(g, weights = NA)$vector
+# as <- igraph::authority_score(g, weights = NA)$vector
+# par(mfrow = c(1,2))
+# plot(g, vertex.size = hs * 10, main = 'Hubs',
+#      vertex.color = rainbow(50))
+# plot(g, vertex.size = as * 9, main = 'Authorities',
+#      vertex.color = rainbow(50))
+# par(mfrow = c(1,1))
+# # Highlighting degrees
+# igraph::V(g)$label.cex <- 2.2 * igraph::V(g)$degree / max(igraph::V(g)$degree) + 0.3
+# igraph::V(g)$label.color <- rgb(0, 0, .2, .8)
+# igraph::V(g)$frame.color <- NA
+# egam <- (log(igraph::E(g)$weight) + .4) / max(log(igraph::E(g)$weight) + .4)
+# igraph::E(g)$color <- rgb(.5, .5, 0, egam)
+# igraph::E(g)$width <- egam
+# plot(g, vertex.size = igraph::V(g)$degree * .5)
