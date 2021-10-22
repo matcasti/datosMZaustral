9d00f236f219ec8cccb2e22e97fc76e14f8a59ab
matcasti <matcasti@umag.cl>
2021-10-22 11:00:31 -0300

    -

---
+++ script/lab_instrumentos/productos/sankey/data_sankey.R
@@ -0,0 +1,29 @@
+## función: Crear diagrama Sankey
+## fecha: 22-oct
+
+# Preparar espacio ----------------------------------------------------------------------------
+
+library(glue);
+library(data.table);
+.s <- function(x, ...) {
+  stopifnot(inherits(x, "data.table"))
+  temp <- substitute(x[...])
+  eval(temp)
+}
+
+# Importar datos ------------------------------------------------------------------------------
+
+data <- readRDS(file = "data/lab_instrumentos/clean/data.RDS")
+
+# Pre-procesamiento ---------------------------------------------------------------------------
+
+.var <- c("laboratorio", "grupo", "norm_problema")
+sankey_data <- highcharter::data_to_sankey(data = copy(data)[, .SD, .SDcols = .var])
+
+# Producto ------------------------------------------------------------------------------------
+
+names <- readLines(con = "script/lab_instrumentos/productos/sankey/assets/names.txt")
+  
+for(i in nrow(sankey_data)) {
+  
+}
