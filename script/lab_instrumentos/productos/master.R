d66bdf32b971e50e426dc9ed6a2c261fdd01730e
matcasti <matcasti@umag.cl>
2021-10-28 11:07:20 -0300

    -

--- script/lab_instrumentos/productos/master.R
+++ script/lab_instrumentos/productos/master.R
@@ -8,6 +8,6 @@ carpeta <- paste0("script/lab_instrumentos/productos/", c("problemas/", "causas/
 for (i in carpeta) {
   archivos <- paste0(i, list.files(i))
   for (i in archivos) {
-    source(file = i) |> suppressWarnings()
+    source(file = i) 
   }
 }
