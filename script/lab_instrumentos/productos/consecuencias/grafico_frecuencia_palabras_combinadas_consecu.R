ee1759c33d51a1e3d4f994aab374b31c63f53fea
carlosmoralesq <csmoarles@minciencia.gob.cl>
2021-10-28 08:59:11 -0300

    Scripts para EDA en consecuencias y exportar a google sheets matriz ANID MZ Austral

---
+++ script/lab_instrumentos/productos/consecuencias/grafico_frecuencia_palabras_combinadas_consecu.R
@@ -0,0 +1,99 @@
+## función: Generar grafico de frecuencia de palabras (CAUSAS)
+## fecha: 26-oct
+
+# Preparar el espacio de trabajo --------------------------------------------------------------
+
+library(data.table)
+library(tm)
+library(RWeka)
+
+# Importar los datos --------------------------------------------------------------------------
+
+data <- readRDS(file = "data/lab_instrumentos/clean/data.RDS")
+
+# Funciones auxiliares ------------------------------------------------------------------------
+
+## Funciones para tokenizar grupos de dos palabras
+twogramTokenizer <- function(x) {
+  NGramTokenizer(
+    x = x, 
+    control = Weka_control(
+      min = 2, 
+      max = 2
+    )
+  )
+}
+
+## Funciones para tokenizar grupos de tres palabras
+threegramTokenizer <- function(x) {
+  NGramTokenizer(
+    x = x, 
+    control = Weka_control(
+      min = 3, 
+      max = 3
+    )
+  )
+}
+
+# Producto ------------------------------------------------------------------------------------
+
+# Creamos un corpus
+m <- tm::VCorpus(
+  x = tm::VectorSource(
+    x = unique(data$normalizacion_causa)
+  )
+)
+
+## Gráfico con top 30 combinaciones de dos palabras
+pdf(file = "output/lab_instrumentos/productos/freq_palabras_combinadas_2_causas.pdf", width = 10, height = 6)
+local({
+  ## Matriz de documentos con dos términos
+  dtm_twogram <- DocumentTermMatrix(
+    x = m, 
+    ### Aplicamos función tokenizadora
+    control = list(tokenize = twogramTokenizer)
+  )
+  
+  ## Creamos el conteo de palabras por dos términos
+  twogram_freq <- dtm_twogram |> 
+    as.matrix() |> 
+    colSums() |> 
+    sort(decreasing = TRUE)
+  
+  ## Generamos el gráfico
+  par(mai = c(1,2.7,1,1))
+  barplot(twogram_freq[1:30],
+          xlab = 'Frencuencia',
+          main = 'Top 30 combinaciones de 2 palabras',
+          names.arg = names(twogram_freq)[1:30],
+          col = 'red3', las = 2, cex.names = .7,
+          horiz = TRUE, las = 1)
+})
+dev.off()
+
+## Gráfico con top 30 combinaciones de tres palabras
+pdf(file = "output/lab_instrumentos/productos/freq_palabras_combinadas_3_CAUSAS.pdf", width = 10, height = 6)
+local({
+  ## Matriz de documentos con dos términos
+  dtm_twogram <- DocumentTermMatrix(
+    x = m, 
+    ### Aplicamos función tokenizadora
+    control = list(tokenize = threegramTokenizer)
+  )
+  
+  ## Creamos el conteo de palabras por dos términos
+  twogram_freq <- dtm_twogram |> 
+    as.matrix() |> 
+    colSums() |> 
+    sort(decreasing = TRUE)
+  
+  ## Generamos el gráfico
+  par(mai = c(1,4,1,1))
+  barplot(twogram_freq[1:30],
+          xlab = 'Frencuencia',
+          main = 'Top 30 combinaciones de 3 palabras',
+          names.arg = names(twogram_freq)[1:30],
+          col = 'lightblue', las = 2, cex.names = .7,
+          horiz = TRUE, las = 1)
+})
+dev.off()
