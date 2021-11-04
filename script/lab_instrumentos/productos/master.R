1548f9980d22e716cf4d7e33d33c590eb01b70d7
matcasti <matcasti@umag.cl>
2021-11-04 10:58:33 -0300

    generación de datos en google sheets para integración con data studio

--- script/lab_instrumentos/productos/master.R
+++ script/lab_instrumentos/productos/master.R
@@ -11,3 +11,41 @@ for (i in carpeta) {
     source(file = i) 
   }
 }
+
+# Un palabra ----------------------------------------------------------------------------------
+
+wordcloud_data <- rbind.data.frame(
+  cbind(problema, `P-C-C` = "problema"),
+  cbind(causas, `P-C-C` = "causa"),
+  cbind(consecuencias, `P-C-C` = "consecuencia"),
+  make.row.names = FALSE
+)
+
+## Guardamos los archivos
+googlesheets4::write_sheet(data = wordcloud_data, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "ccp")
+
+
+# Dos palabras --------------------------------------------------------------------------------
+
+twoword_data <- rbind.data.frame(
+  data.frame(WORD = names(twogram_freq_problema), FREQ = as.numeric(twogram_freq_problema), `P-C-C` = "problema", check.names = FALSE),
+  data.frame(WORD = names(twogram_freq_causas), FREQ = as.numeric(twogram_freq_causas), `P-C-C` = "causas", check.names = FALSE),
+  data.frame(WORD = names(twogram_freq_consecuencias), FREQ = as.numeric(twogram_freq_consecuencias), `P-C-C` = "consecuencia", check.names = FALSE),
+  make.row.names = FALSE
+)
+
+## Guardamos los archivos
+googlesheets4::write_sheet(data = twoword_data, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "two-words")
+
+# Tres palabras --------------------------------------------------------------------------------
+
+threeword_data <- rbind.data.frame(
+  data.frame(WORD = names(threegram_freq_problema), FREQ = as.numeric(threegram_freq_problema), `P-C-C` = "problema", check.names = FALSE),
+  data.frame(WORD = names(threegram_freq_causas), FREQ = as.numeric(threegram_freq_causas), `P-C-C` = "causas", check.names = FALSE),
+  data.frame(WORD = names(threegram_freq_consecuencias), FREQ = as.numeric(threegram_freq_consecuencias), `P-C-C` = "consecuencia", check.names = FALSE),
+  make.row.names = FALSE
+)
+
+## Guardamos los archivos
+googlesheets4::write_sheet(data = threeword_data, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "three-words")
+
