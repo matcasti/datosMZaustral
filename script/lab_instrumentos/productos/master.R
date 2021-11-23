d6d9e7513dd6ca312472d85226181c14cf4e98cc
matcasti <matcasti@umag.cl>
2021-11-23 12:00:58 -0300

    Inclusión de soluciones

--- script/lab_instrumentos/productos/master.R
+++ script/lab_instrumentos/productos/master.R
@@ -9,7 +9,7 @@ library(labinstrumentos)
 lab_instrumentos <- obtener_datos()[region == 1]
 
 # Variables de interés
-vars <- c("clean_problema", "clean_causa", "clean_consecuencia")
+vars <- c("clean_problema", "clean_causa", "clean_consecuencia", "clean_soluciones")
 
 # Eliminamos el término macrozona-austral por se muy influyente
 lab_instrumentos[j = (vars) := lapply(.SD, gsub, pattern = "macrozona-austral", replacement = ""), 
@@ -17,7 +17,7 @@ lab_instrumentos[j = (vars) := lapply(.SD, gsub, pattern = "macrozona-austral", 
 
 lab_instrumentos_long <- melt.data.table(
   data = lab_instrumentos,
-  measure.vars = c("clean_problema", "clean_causa", "clean_consecuencia")
+  measure.vars = c("clean_problema", "clean_causa", "clean_consecuencia", "clean_soluciones")
 )
 
 # dcast.data.table(
@@ -29,23 +29,24 @@ lab_instrumentos_long <- melt.data.table(
 # Una palabra
 
 one <- lab_instrumentos_long[, obtener_terminos(value, n_words = 1), .(variable)
-][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia")))]
+][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia", "Soluciones")))]
+
+one[WORD == "anid", sum(FREQ)];
 
 write_sheet(data = one, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "ccp")
-
 
 
 # Dos palabra
 
 two <- lab_instrumentos_long[, obtener_terminos(value, n_words = 2), .(variable)
-][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia")))]
+][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia", "Soluciones")))]
 write_sheet(data = two, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "two-words")
 
 
 # tres palabra
 
 three <- lab_instrumentos_long[, obtener_terminos(value, n_words = 3), .(variable)
-][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia")))]
+][, .(WORD, FREQ, `P-C-C` = `levels<-`(variable, c("Problema", "Causa", "Consecuencia", "Soluciones")))]
 write_sheet(data = three, ss = "16IGJFT63uVC0uerATaEOS6NK4IJ7iOLQ6xoBmIowz58", sheet = "three-words")
 
 
